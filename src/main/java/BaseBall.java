dc2d341e838037a7ea1a20509b01f53e3110d752
“ggmcjy94” <lhs01071526524@gmail.com>
2022-07-04 15:49:53 +0900

    factory-pattern

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -3,5 +3,6 @@ public class BaseBall  implements Ball{
     public void make() {
         System.out.println("야구공 제작!!!");
         System.out.println("asdasdasdasd");
+        System.out.println("asdasdasdasdasd");
     }
 }
